5d4094630aeae1a3c2ff8c4bf2fa9e120d32d318
Dhruva Bansal <dbansal36@gatech.edu>
2021-05-01 02:39:14 -0400

    Change to summary

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.120e8aa8e81a5adee2dfeafea7472e55.js"
+  "/precache-manifest.ef8f43ea7fb7eb67aedf1e69a59bf9b8.js"
 );
 
 self.addEventListener('message', (event) => {
