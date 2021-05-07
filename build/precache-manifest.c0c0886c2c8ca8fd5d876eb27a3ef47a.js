54b6411bd257bf1d793ac99895932d72925eb469
Dhruva Bansal <dbansal36@gatech.edu>
2021-05-07 00:48:46 -0400

    Remove github from navbar

---
+++ build/precache-manifest.c0c0886c2c8ca8fd5d876eb27a3ef47a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9abaa18befbd5beade0b9fc08e2192",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68b4f91d314c1fb8ebe1",
+    "url": "/static/js/2.02919864.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.02919864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e82007b969d5d8c3dc",
+    "url": "/static/js/main.c63e2968.chunk.js"
+  },
+  {
+    "revision": "0b4f3269348c2e5315e5",
+    "url": "/static/js/runtime-main.9089ee33.js"
+  }
+]);
--- src/components/elements/NavBar.js
+++ src/components/elements/NavBar.js
@@ -41,7 +41,6 @@ class NavBar extends React.Component {
               <MenuItem text="About Me" href="#aboutMe" />
               <MenuItem text="Experience" href="#experience" />
               <MenuItem text="Projects" href="#projects" />
-              <MenuItem text="GitHub Contributions" href="#github" />
               <MenuItem text="Skills" href="#skills" />
             { // <MenuItem text="Articles" href="#articles" />
             }
