866aecbe517074e20abb923b37edbbbb2fd98f6c
Dhruva Bansal <dbansal36@gatech.edu>
2021-05-07 00:33:26 -0400

    Awkward space on mobile removed

--- src/components/sections/GitHub.js
+++ src/components/sections/GitHub.js
@@ -3,8 +3,8 @@ import GitHubCalendar from 'react-github-calendar';
 import ReactTooltip from 'react-tooltip';
 function GitHub() {
   return (
-    <section className="section" id="github">
-      <div className="container is-centered github">
+    <section className="section github" id="github">
+      <div className="container is-centered">
         <h1 className="title">GitHub Contributions:</h1>
         <GitHubCalendar username="DhruvaBansal00" blockSize={24} fontSize={20}>
           <ReactTooltip delayShow={50} html textColor="black" backgroundColor="white"/>
